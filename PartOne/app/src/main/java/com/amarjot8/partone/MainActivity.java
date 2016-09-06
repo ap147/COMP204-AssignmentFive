37697bfaf0eec00153aaeb9dc10294285816bc51
Parmar, Amarjot <amarjot-parmar@hotmail.co.nz>
2016-09-07 02:56:52 +1200

    Problem encountered, "cant resolve activity_display_message"

--- PartOne/app/src/main/java/com/amarjot8/partone/MainActivity.java
+++ PartOne/app/src/main/java/com/amarjot8/partone/MainActivity.java
@@ -1,13 +1,28 @@
 package com.amarjot8.partone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_Message = "Amarjot Parmar (1255668)";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText =(EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_Message,message);
+        startActivity(intent);
+        // Do something in response to button
+    }
+
 }
